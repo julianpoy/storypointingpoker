2d01ad1fffe6309fbf50846b3a071ca598fbd42c
Julian Poyourow <julianpoyo@gmail.com>
2021-02-05 20:55:59 -0800

    Add some example routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,72 @@
 const express = require('express');
-const cors = require('cors');
 const morgan = require('morgan');
 const os = require('os');
+const path = require('path');
+const pug = require('pug');
 
 const app = express();
 app.use(morgan(':method :url :status :response-time ms'));
 app.use(express.json());
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use('/', (req, res) => res.render('frontend'));
+
 const rooms = {};
 
-app.get('/room/:id', async (req, res) => {
+app.get('/rooms/:id', (req, res) => {
   const room = rooms[req.params.id];
 
-  if (!room) res.status(404).send("Not found");
+  if (!room) return res.sendStatus(404);
+
+  res.status(200).send(room);
+});
+
+const ROOM_CODE_LENGTH = 5;
+const generateRoomCode = () => {
+  const alphabet = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
+  const alphabetLength = alphabet.length;
+
+  let roomCode = '';
+  for (let i = 0; i < ROOM_CODE_LENGTH; i++) {
+    roomCode += alphabet.charAt(Math.floor(Math.random() * alphabetLength));
+  }
+
+  if (!rooms[roomCode]) {
+    return roomCode;
+  }
+
+  return generateRoomCode();
+}
+
+app.post('/rooms', (req, res) => {
+  const room = {
+    code: generateRoomCode(),
+    members: [],
+    state: 'start',
+  };
+
+  rooms[room.code] = room;
 
   res.status(200).send();
+});
+
+app.post('/rooms/:id/join', (req, res) => {
+  const { socketClientId, name } = req.body;
+
+  if (!socketClientId || !name) return res.sendStatus(400);
+
+  const room = rooms[req.params.id];
+
+  if (!room) return res.sendStatus(404);
+
+  room.members.push({
+    socketClientId,
+    name
+  });
+  
+  res.sendStatus(200);
 });
 
 const server = app.listen(parseInt(process.env.PORT || 3000, 10));
