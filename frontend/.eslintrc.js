b1caa14ae84ce0a21f03313782961ea470cb1c8d
Julian Poyourow <julianpoyo@gmail.com>
2021-02-06 08:41:47 -0800

    Add eslint, build commands, socket.io

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    "one-var": 0,
+    "one-var-declaration-per-line": ["error", "initializations"],
+    "no-param-reassign": 0,
+    "import/prefer-default-export": 0,
+    "import/no-self-import": 0,
+  },
+};
